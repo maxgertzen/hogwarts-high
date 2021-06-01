060aebf36420b5387bf94fb746f52d6a570ab890
maxgertzen <maxgertzen@gmail.com>
2021-06-01 15:21:11 +0300

    form pagination component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Phase1 from "./components/Phase1"
 import Phase2 from "./components/Phase2"
 import Phase3 from "./components/Phase3"
-import Summary from "./components/Summary"
+import Summary from "./components/Summary";
+import FormPagination from "./components/FormPagination/FormPagination"
+
 
 function App() {
   const [inputData, setInputData] = useState({})
   const [presentedPhase, setPresentedPhase] = useState(0)
 
   const handleNext = (dataObj) => {
-    // console.table(dataObj)
     setInputData((prevObj) => ({
       ...prevObj,
       ...dataObj,
@@ -22,7 +23,6 @@ function App() {
   }
 
   const handlePrev = (dataObj) => {
-    // console.table(dataObj)
     setInputData((prevObj) => ({
       ...prevObj,
       ...dataObj,
@@ -36,12 +36,15 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/">
+            <FormPagination />
             <Phase1 onNextPhase={handleNext} />
           </Route>
           <Route exact path="/phase-2">
+            <FormPagination />
             <Phase2 onNextPhase={handleNext} prevPhase={handlePrev} />
           </Route>
           <Route exact path="/phase-3">
+            <FormPagination />
             <Phase3 onNextPhase={handleNext} prevPhase={handlePrev} />
           </Route>
           <Route exact path="/summary">
---
+++ src/components/FormPagination/FormPagination.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useRouteMatch } from "react-router-dom";
+import './FormPagination.css';
+
+function FormPagination() {
+    let { url: item } = useRouteMatch()
+    item = item.match(/(\d+|\/+)/).join('');
+    return (
+        <div className="path-container mx-auto my-2">
+            {!item ? null :
+                ['/', '2', '3'].map((value, index) => {
+                    return <div className={`dot${item === value ? ' active' : ''}`} key={Math.round((index + 1) * Math.random() * 100000)}></div>
+                })
+            }
+        </div>
+    )
+}
+
+export default FormPagination
