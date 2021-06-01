39e7d8af29640fd4d4d3dcef17d5d72c49bfc401
“liadbelade” <liadb13@gmail.com>
2021-06-01 14:19:49 +0300

    finished styling summary phase

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
     }
   }
   return (
-    <div className="App">
+    <div className="App mt-5">
       <Router>
         <Switch>
           <Route exact path="/">
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react"
-import { Row, Image, ListGroup, Card, Container, Col } from "react-bootstrap"
+import { Row, Image, ListGroup, Container, Col } from "react-bootstrap"
 
 // import img from './'
 function Summary({ data }) {
@@ -12,31 +12,37 @@ function Summary({ data }) {
   console.table(data)
   return (
     <>
-      <Container>
+      <Container className="d-flex flex-column align-items-center my-2">
+        <h2 className="text-dark">Summary</h2>
         <Row>
-          <Col xs={6} md={4}>
+          <Col className="my-3">
             <Image
               src={data.image.value}
-              style={{ borderRadius: "50%", objectFit: "contain" }}
-              height="250px"
-              width="250px"
+              style={{ clipPath: "circle()" }}
+              height="200px"
+              width="200px"
               fluid
             />
           </Col>
-          <Col xs={6} md={4}>
-            <Card style={{ width: "18rem" }}>
-              <ListGroup variant="flush">
-                <ListGroup.Item>Name: {data.fullname.value}</ListGroup.Item>
-                <ListGroup.Item>Email: {data.email.value}</ListGroup.Item>
-                <ListGroup.Item>
-                  Birth Date: {data.birthDate.value}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  Address: {data.city.value} - {data.street.value}
-                </ListGroup.Item>
-                {/* <ListGroup.Item>Hobbies: {data.hobbie}</ListGroup.Item> */}
-              </ListGroup>
-            </Card>
+        </Row>
+        <Row>
+          <Col>
+            <ListGroup variant="flush">
+              <ListGroup.Item className="mt-2">
+                <strong> Name: </strong> {data.fullname.value}
+              </ListGroup.Item>
+              <ListGroup.Item className="mt-2">
+                <strong> Email: </strong> {data.email.value}
+              </ListGroup.Item>
+              <ListGroup.Item className="mt-2">
+                <strong> Birth Date: </strong> {data.birthDate.value}
+              </ListGroup.Item>
+              <ListGroup.Item className="mt-2">
+                <strong> Address: </strong> {data.city.value} -
+                {data.street.value}
+              </ListGroup.Item>
+              {/* <ListGroup.Item>Hobbies: {data.hobbie}</ListGroup.Item> */}
+            </ListGroup>
           </Col>
         </Row>
       </Container>
@@ -44,15 +50,3 @@ function Summary({ data }) {
   )
 }
 export default Summary
-// 1- Name       (required, 2 words (fn + ln), minimum 2 characters each)
-// 2- Email      (required, valid email address)
-// 3- Birth Date (required, dd/MM/yy)
-// Phase 2:
-// --------
-// 4- City   (required)
-// 5- Street (required)
-// 6- Number (not required, not 0 or negative)
-// Phase 3:
-// --------
-// 7- Image   (required, valid url)
-// 8- Hobbies (not required. Chess, Movies, Sport, Cars, Dolls)
