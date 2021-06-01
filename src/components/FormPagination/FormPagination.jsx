5ec3c75224ea2dce850f6282a14faa8088afa5dd
maxgertzen <maxgertzen@gmail.com>
2021-06-01 15:39:20 +0300

    form-pagination bugfix styled the dots

--- src/components/FormPagination/FormPagination.jsx
+++ src/components/FormPagination/FormPagination.jsx
@@ -3,12 +3,14 @@ import { useRouteMatch } from "react-router-dom";
 import './FormPagination.css';
 
 function FormPagination() {
+    // const [item, setItem] = useState('')
     let { url: item } = useRouteMatch()
     item = item.match(/(\d+|\/+)/).join('');
+
     return (
         <div className="path-container mx-auto my-2">
             {!item ? null :
-                ['/', '2', '3'].map((value, index) => {
+                ['//', '2', '3'].map((value, index) => {
                     return <div className={`dot${item === value ? ' active' : ''}`} key={Math.round((index + 1) * Math.random() * 100000)}></div>
                 })
             }
