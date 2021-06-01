b7cdcd85665441a3006ebd9f9b478a4798ae2e90
maxgertzen <maxgertzen@gmail.com>
2021-06-01 10:26:20 +0300

    added useLocalStorage hook

--- src/components/Phase2.jsx
+++ src/components/Phase2.jsx
@@ -1,24 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import { validateDataOnSubmit } from '../validations';
 import FormErrorMessages from "./FormErrorMessages"
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function Phase2({ onNextPhase, prevPhase }) {
+    const [storedCity, setStoredCity] = useLocalStorage('city', '')
+    const [storedStreet, setStoredStreet] = useLocalStorage('street', '')
+    const [storedStreetNum, setStoredStreetNum] = useLocalStorage('streetNumber', '')
     const [formProps, setFormProps] = useState({
         city: {
-            value: '',
+            value: storedCity,
             errors: []
         },
         street: {
-            value: '',
+            value: storedStreet,
             errors: []
         },
         streetNumber: {
-            value: '',
+            value: storedStreetNum,
             errors: []
         },
     });
+
 
     const handleChange = ({ target: { name, value } }, func) => {
         func((prevObj) => {
@@ -27,6 +32,9 @@ function Phase2({ onNextPhase, prevPhase }) {
                 [name]: value
             }
         });
+        if (name === 'city') setStoredCity(value);
+        if (name === 'street') setStoredStreet(value);
+        if (name === 'streetNumber') setStoredStreetNum(value);
     }
 
     const handleNext = (e) => {
@@ -51,17 +59,17 @@ function Phase2({ onNextPhase, prevPhase }) {
         <Form>
             <Form.Group controlId="phase2-city">
                 <Form.Label>City</Form.Label>
-                <Form.Control type="text" placeholder="Enter City" name="city" onChange={e => handleChange(e, setFormProps)} required />
+                <Form.Control type="text" placeholder="Enter City" name="city" onBlur={e => handleChange(e, setFormProps)} required />
                 <FormErrorMessages errors={formProps.city.errors} />
             </Form.Group>
             <Form.Group controlId="phase2-street">
                 <Form.Label>Street</Form.Label>
-                <Form.Control type="text" placeholder="Enter Street" name="street" onChange={e => handleChange(e, setFormProps)} required />
+                <Form.Control type="text" placeholder="Enter Street" name="street" onBlur={e => handleChange(e, setFormProps)} required />
                 <FormErrorMessages errors={formProps.street.errors} />
             </Form.Group>
             <Form.Group controlId="phase3-number">
                 <FormErrorMessages errors={formProps.streetNumber.errors} />
-                <Form.Control type="number" label="N" name="streetNumber" onChange={e => handleChange(e, setFormProps)} />
+                <Form.Control type="number" label="N" name="streetNumber" onBlur={e => handleChange(e, setFormProps)} />
             </Form.Group>
             <Button variant="success" type="button" onClick={(e) => handleNext(e)}>
                 Next
