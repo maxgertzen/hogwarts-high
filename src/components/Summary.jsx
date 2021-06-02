0f004cd0e1e280ca28454e8fd409927b2c159e21
“liadbelade” <liadb13@gmail.com>
2021-06-02 22:58:20 +0300

    summary styling and install react icons

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -11,38 +11,48 @@ function Summary({ data }) {
 
   return (
     <>
-      <Container className="d-flex flex-column align-items-center my-2">
-        <h2 className="text-dark">Summary</h2>
-        <Row>
-          <Col className="my-3">
+      <Container fluid className="d-flex flex-column align-items-center  p-2">
+        <h2 className="text-dark mb-2">Summary</h2>
+        <Row className="w-100">
+          <Col md={5} className="mt-2 ml-3">
             <Image
               src={data.image.value}
               style={{ clipPath: "circle()" }}
               height="200px"
               width="200px"
-              fluid
             />
           </Col>
-        </Row>
-        <Row>
-          <Col>
+          <Col md={7} className="ml-3">
             <ListGroup variant="flush">
               <ListGroup.Item className="mt-2">
-                <strong> Name: </strong> {data['full-name'].value}
+                <strong> Name: </strong> {data["full-name"].value}
               </ListGroup.Item>
               <ListGroup.Item className="mt-2">
                 <strong> Email: </strong> {data.email.value}
               </ListGroup.Item>
               <ListGroup.Item className="mt-2">
-                <strong> Birth Date: </strong> {data['birth-date'].value}
+                <strong> Birth Date: </strong> {data["birth-date"].value}
               </ListGroup.Item>
               <ListGroup.Item className="mt-2">
                 <strong> Address: </strong> {data.city.value} -
                 {data.street.value}
               </ListGroup.Item>
-              <ListGroup.Item>Hobbies: </ListGroup.Item>
             </ListGroup>
           </Col>
+        </Row>
+        <Row className="w-100 text-center mt-3">
+          {data.hobbies.length > 0 && (
+            <>
+              <h4> Hobbies </h4>
+              <Col className="d-flex justify-content-evenly my-3">
+                {data.hobbies.map((hobby, idx) => (
+                  <ListGroup.Item className="text-capitalize" key={idx}>
+                    {hobby}
+                  </ListGroup.Item>
+                ))}
+              </Col>
+            </>
+          )}
         </Row>
       </Container>
     </>
